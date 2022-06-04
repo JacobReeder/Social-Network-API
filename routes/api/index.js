80c037a05b1a9d43e701cdce80c91711d671461f
JacobReeder <jacob.reeder1@gmail.com>
2022-06-04 12:52:49 -0700

    Removed pseudocode

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
-// add prefix of `/pizzas` to routes created in `user-routes.js`
+
 router.use('/comments', thoughtRoutes);
 router.use('/users', userRoutes);
 
