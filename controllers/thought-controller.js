e21ca7f3c628245bea6ab3650f2a641930bfa89e
JacobReeder <jacob.reeder1@gmail.com>
2022-06-05 14:06:13 -0700

    Added reaction functions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -75,8 +75,28 @@ const thoughtController = {
         res.json(dbUserData);
       })
       .catch(err => res.json(err));
-    }
+    },
 
+////Also add reactions
+////.post
+addReaction({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.status(400).json(err));
+ },
+
+///.delete
+deleteReaction({ params }, res) {
+  Thought.findOneAndDelete({ _id: params.id })
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No reaction found with this id!' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+  }
 };
 
 module.exports = thoughtController;
