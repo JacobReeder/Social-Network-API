8cbb829d30705302c6f60e2ec077cb3c28e17a3d
JacobReeder <jacob.reeder1@gmail.com>
2022-06-06 14:20:22 -0700

    Removed some pseudocode

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -39,17 +39,4 @@ router
  .post(postFriend) // to add a new friend to a user's friend list
  .delete(deleteFriend) // to remove a friend from a user's friend list*/
 
-
-// -- Directs to: /api/users <GET, POST>
-//router.route('/').get(getAllUsers).post(createUser);
-
-// -- Directs to: /api/users/:id <GET, PUT, DELETE>
-//router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
-
-// -- Directs to: /api/users/:userId/friends/:friendId <POST, DELETE>
-//router.route('/:id/friends/:friendId').post(postFriend).delete(deleteFriend)
-
-
-
-
 module.exports = router;
